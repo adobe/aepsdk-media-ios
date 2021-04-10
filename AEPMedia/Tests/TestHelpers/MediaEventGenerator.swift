882f953bc83eb36b8397e1abcded478650ff81eb
Ryan Morales <rymorale@adobe.com>
2021-04-09 19:29:57 -0700

    format

--- AEPMedia/Tests/TestHelpers/MediaEventGenerator.swift
+++ AEPMedia/Tests/TestHelpers/MediaEventGenerator.swift
@@ -114,11 +114,11 @@ class MediaEventGenerator: MediaTracker {
             return
         }
     }
-    
+
     func getCurrentTimeStamp() -> TimeInterval {
         return tracker.mockTimeStamp
     }
-    
+
     func getLastEventTimeStamp() -> TimeInterval {
         return tracker.lastEventTs
     }
