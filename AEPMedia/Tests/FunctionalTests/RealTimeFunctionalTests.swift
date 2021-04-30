e9219a878b8f5829b9f26069c83762dbdbb2b9a1
Arjun Bhadra <abhadra@adobe.com>
2021-04-30 16:13:47 -0700

    Added functional tests for MediaRealTimeSession

---
+++ AEPMedia/Tests/FunctionalTests/RealTimeFunctionalTests.swift
@@ -0,0 +1,184 @@
+/*
+ Copyright 2021 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+import XCTest
+import AEPCore
+import AEPIdentity
+import AEPServices
+@testable import AEPMedia
+
+class RealTimeFunctionalTests: MediaFunctionalTestBase {
+    var session: MediaRealTimeSession!
+    var mediaState: MediaState!
+    var fakeMediaService: FakeMediaService!
+    var dispatchQueue: DispatchQueue!
+    var mockMediaData: MockMediaData!
+
+    override func setUp() {
+        super.setupBase()
+        mockMediaData = MockMediaData()
+        dispatchQueue = DispatchQueue(label: "testRealTime")
+
+        mediaState = MediaState()
+        mediaState.extractConfigurationInfo(from: mockMediaData.configSharedState)
+        mediaState.extractIdentityInfo(from: mockMediaData.identitySharedState)
+        mediaState.extractAnalyticsInfo(from: mockMediaData.analyticsSharedState)
+
+        session = MediaRealTimeSession(id: "sessionID", state: mediaState, dispatchQueue: dispatchQueue)
+        session.retryDuration = 0
+    }
+
+    func testTrySendHit_success() {
+        // setup
+        mockNetworkService.expectedResponse = HttpConnection(data: nil, response: HTTPURLResponse(url: URL(string: "https://www.adobe.com")!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: ["Location": "/api/test/sessions/MediaCollectionServerSessionId"]), error: nil)
+
+        // test
+        session.handleQueueMediaHit(hit: mockMediaData.sessionStart)
+        waitForProcessing()
+
+        let requests = mockNetworkService.calledNetworkRequests
+        XCTAssertEqual(1, requests.count)
+        XCTAssertTrue(mockNetworkService.connectAsyncCalled)
+    }
+
+    func testTrySendHit_ConnectionError_fail_shouldRetry3times() {
+        // setup
+        mockNetworkService.shouldReturnConnectionError = true
+
+        // test
+        session.handleQueueMediaHit(hit: mockMediaData.sessionStart)
+        waitForProcessing()
+
+        // verify
+        let requests = mockNetworkService.calledNetworkRequests
+        XCTAssertEqual(0, session.hits.count)
+        XCTAssertEqual(3, requests.count)
+        XCTAssertTrue(mockNetworkService.connectAsyncCalled)
+    }
+
+    func testTrySendHit_ConnectionError_PassAfter2Retries_UnableToExtractMCSessionID_ShouldNotDeleteHitFromSession() {
+        // setup
+        session.retryDuration = 1
+        mockNetworkService.shouldReturnConnectionError = true
+
+        // test
+        session.handleQueueMediaHit(hit: mockMediaData.sessionStart)
+        // 1st try
+        waitForProcessing(interval: 1)
+        // 2nd try
+        waitForProcessing(interval: 1)
+
+        // make network request pass
+        mockNetworkService.shouldReturnConnectionError = false
+
+        // 3rd try
+        waitForProcessing(interval: 1)
+
+        // verify
+        let requests = mockNetworkService.calledNetworkRequests
+        XCTAssertEqual(1, session.hits.count)
+        XCTAssertEqual(3, requests.count)
+        XCTAssertTrue(mockNetworkService.connectAsyncCalled)
+    }
+
+    func testTrySendHit_ConnectionError_PassAfter2Retries_ExtractMCSessionID_DeleteHitFromSession() {
+        // setup
+        session.retryDuration = 1
+        mockNetworkService.shouldReturnConnectionError = true
+        mockNetworkService.expectedResponse = HttpConnection(data: nil, response: HTTPURLResponse(url: URL(string: "https://www.adobe.com")!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: ["Location": "/api/test/sessions/MediaCollectionServerSessionId"]), error: nil)
+
+        // test
+        session.handleQueueMediaHit(hit: mockMediaData.sessionStart)
+        // 1st try
+        waitForProcessing(interval: 1)
+        // 2nd try
+        waitForProcessing(interval: 1)
+
+        // make network request pass
+        mockNetworkService.shouldReturnConnectionError = false
+
+        // 3rd try
+        waitForProcessing(interval: 1)
+
+        // verify
+        let requests = mockNetworkService.calledNetworkRequests
+        XCTAssertEqual(0, session.hits.count)
+        XCTAssertEqual(3, requests.count)
+        XCTAssertTrue(mockNetworkService.connectAsyncCalled)
+    }
+
+    func testTrySendHit_ConnectionError_ShouldNotRetryAfterMaxRetries() {
+        // setup
+        session.retryDuration = 1
+        mockNetworkService.shouldReturnConnectionError = true
+
+        // test
+        session.handleQueueMediaHit(hit: mockMediaData.sessionStart)
+        // 1st try
+        waitForProcessing(interval: 1)
+
+        // make network request pass
+        mockNetworkService.shouldReturnConnectionError = false
+
+        // 2nd try
+        waitForProcessing(interval: 1)
+
+        // verify
+        let requests = mockNetworkService.calledNetworkRequests
+        XCTAssertEqual(2, requests.count)
+        XCTAssertTrue(mockNetworkService.connectAsyncCalled)
+    }
+
+    func testTrySendHitSessionStart_MissingSessionIDResponse_MaxRetries_DropHits() {
+        // setup
+        mockNetworkService.expectedResponse = HttpConnection(data: nil, response: HTTPURLResponse(url: URL(string: "https://www.adobe.com")!, statusCode: 200, httpVersion: nil, headerFields: [:]), error: nil)
+
+        // test
+        session.handleQueueMediaHit(hit: mockMediaData.sessionStart)
+        session.handleQueueMediaHit(hit: mockMediaData.play)
+        waitForProcessing(interval: 1)
+
+        // verify
+        let requests = mockNetworkService.calledNetworkRequests
+        // dropeed the hits
+        XCTAssertEqual(0, session.hits.count)
+        // play ping not sent
+        XCTAssertEqual(3, requests.count)
+        XCTAssertTrue(mockNetworkService.connectAsyncCalled)
+    }
+
+    func testTrySendHit_Play_Success() {
+        // setup
+        mockNetworkService.expectedResponse = HttpConnection(data: nil, response: HTTPURLResponse(url: URL(string: "https://www.adobe.com")!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: ["Location": "/api/test/sessions/MediaCollectionServerSessionId"]), error: nil)
+
+        // test
+        session.handleQueueMediaHit(hit: mockMediaData.sessionStart)
+        waitForProcessing(interval: 1)
+        session.handleQueueMediaHit(hit: mockMediaData.play)
+
+        // Play ping
+        waitForProcessing(interval: 1)
+
+        // verify
+        let requests = mockNetworkService.calledNetworkRequests
+        XCTAssertEqual(2, requests.count)
+        XCTAssertTrue(mockNetworkService.connectAsyncCalled)
+
+        let sessionStartRequest = requests[0]
+        let sessionStartRequestURLString = sessionStartRequest?.connectPayload ?? ""
+        let playRequest = requests[1]
+        let playRequestURLString = playRequest?.connectPayload ?? ""
+
+        XCTAssertTrue(sessionStartRequestURLString.contains("\"eventType\":\"sessionStart\""))
+        XCTAssertTrue(playRequest?.url.absoluteString.contains("MediaCollectionServerSessionId") ?? false)
+        XCTAssertTrue(playRequestURLString.contains("\"eventType\":\"play\""))
+    }
+}
